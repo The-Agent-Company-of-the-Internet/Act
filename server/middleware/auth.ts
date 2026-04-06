02180f94a36d6c6a4f00c3f1dac68cedfc39c5e4
Marvellous <marvellousnwachukwu111@gmail.com>
2026-04-07 00:34:56 +0100

    made changes

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,18 +1,10 @@
 // For now let us just remove middleware from the page
 
-// export default defineEventHandler(async (event) => {
-  
-//   if (process.server && import.meta.env.NITRO_PRERENDER) {
-//     return
-//   }
-  
-//   const auth0 = useAuth0(event);
-//   const session = await auth0.getSession();
-//   if (!session) {
-//     return navigateTo("/") // I am not sure this works
-//     throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
-//   }
-// })
-
-
-export default defineEventHandler(async(event) => {})
+export default defineEventHandler(async (event) => {
+  const auth0 = useAuth0(event);
+  const session = await auth0.getSession();
+  if (!session) {
+    return navigateTo("/") // I am not sure this works
+    throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
+  }
+})
