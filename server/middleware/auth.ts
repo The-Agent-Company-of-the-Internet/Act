ab6dbc12691c7389382bba242c73deb62b56ff01
Marvellous <marvellousnwachukwu111@gmail.com>
2026-04-07 00:17:54 +0100

    fixing prerender issues

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,9 @@
 export default defineEventHandler(async (event) => {
+  
+  if (process.server && import.meta.env.NITRO_PRERENDER) {
+    return
+  }
+  
   const auth0 = useAuth0(event);
   const session = await auth0.getSession();
   if (!session) {
