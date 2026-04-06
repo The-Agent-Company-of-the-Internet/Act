833d7948037daa06c9736dd8a4b4d902d4fe8913
Marvellous <marvellousnwachukwu111@gmail.com>
2026-04-07 00:20:52 +0100

    checking fix for prerendering

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,15 @@
-export default defineEventHandler(async (event) => {
+// For now let us just remove middleware from the page
+
+// export default defineEventHandler(async (event) => {
   
-  if (process.server && import.meta.env.NITRO_PRERENDER) {
-    return
-  }
+//   if (process.server && import.meta.env.NITRO_PRERENDER) {
+//     return
+//   }
   
-  const auth0 = useAuth0(event);
-  const session = await auth0.getSession();
-  if (!session) {
-    throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
-  }
-})
+//   const auth0 = useAuth0(event);
+//   const session = await auth0.getSession();
+//   if (!session) {
+//     return navigateTo("/") // I am not sure this works
+//     throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
+//   }
+// })
