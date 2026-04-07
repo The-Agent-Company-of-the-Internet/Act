79228e2e0866ab8a13a457ef0402f95e8f05db53
Marvellous <marvellousnwachukwu111@gmail.com>
2026-04-07 01:04:36 +0100

    made changes to auth middleware in server

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,9 +1,7 @@
-// For now let us just remove middleware from the page
-
 export default defineEventHandler(async (event) => {
-  const auth0 = useAuth0(event);
-  const session = await auth0.getSession();
-  if (!session) {
-    throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
-  }
+  // const auth0 = useAuth0(event);
+  // const session = await auth0.getSession();
+  // if (!session) {
+  //   throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
+  // }
 })
