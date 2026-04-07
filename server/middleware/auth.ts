022cc0e8bb79e89aa345c627428bcc4a2a53e598
Marvellous <marvellousnwachukwu111@gmail.com>
2026-04-07 01:02:13 +0100

    remove navigateTo in router

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -4,7 +4,6 @@ export default defineEventHandler(async (event) => {
   const auth0 = useAuth0(event);
   const session = await auth0.getSession();
   if (!session) {
-    return navigateTo("/") // I am not sure this works
     throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
   }
 })
